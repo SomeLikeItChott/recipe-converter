6749435d0d17fe3490c88f8556bb495540f14553
Sam <samchott@gmail.com>
2024-02-14 14:40:31 -0600

    recorded change

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,6 @@ todo: (not in order, do what calls to you)
 
 accessibility
 make textboxes prettier?
-make hints for advanced options work on mobile (likely just take them out of the tooltip and into small text)
 better error handling (ie make it communicate about what caused the error. or just do anything that's not doing nothing)
 fix convert to mass function (it could be like five different function)
 switch to more comprehensive usda database ( i think it might be at https://fdc.nal.usda.gov/? you have link on other computer)
